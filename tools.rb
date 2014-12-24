91339f9700b7f1603217401f0090119c002398bb
b10011 <nbjarvinen@gmail.com>
2014-12-24 15:39:50 +0200

    Fixed a bug which occurs if you have spaces in the projects' name

--- tools.rb
+++ tools.rb
@@ -28,7 +28,7 @@ class Tools
 		projectlist = Dir.entries(@projects).select do |entry|
 			File.directory? File.join(@projects,entry)
 		end
-		
+
 		projectlist.delete(".")
 		projectlist.delete("..")
 
@@ -70,7 +70,7 @@ class Tools
 		version = "cracked"
 		version = "original" if original
 
-		cmd = eval("`unzip -o #{@projects+@project}/apk/#{version}.apk -d #{@projects+@project}/unzip/#{version}`")
+		cmd = eval("`unzip -o \"#{@projects+@project}/apk/#{version}.apk\" -d \"#{@projects+@project}/unzip/#{version}\"`")
 
 	end
 
@@ -79,7 +79,7 @@ class Tools
 
 		version = "cracked"
 		version = "original" if original
-		cmd = eval("`tools/dex2jar/d2j-dex2jar.sh -ts #{@projects+@project}/unzip/#{version}/classes.dex -o #{@projects+@project}/jar/#{version}.jar`")
+		cmd = eval("`tools/dex2jar/d2j-dex2jar.sh -ts \"#{@projects+@project}/unzip/#{version}/classes.dex\" -o \"#{@projects+@project}/jar/#{version}.jar\"`")
 
 	end
 
@@ -89,19 +89,19 @@ class Tools
 		version = "cracked"
 		version = "original" if original
 
-		cmd = eval("`java -jar tools/jd-core-java.jar #{@projects+@project}/jar/#{version}.jar #{@projects+@project}/java/#{version}`")
+		cmd = eval("`java -jar tools/jd-core-java.jar \"#{@projects+@project}/jar/#{version}.jar\" \"#{@projects+@project}/java/#{version}\"`")
 
 	end
 
 	def apktool_build
 
-		cmd = eval("`java -jar tools/apktool.jar b -f #{@projects+@project}/reversed/cracked/ -o #{@projects+@project}/apk/cracked.apk`")
+		cmd = eval("`java -jar tools/apktool.jar b -f \"#{@projects+@project}/reversed/cracked/\" -o \"#{@projects+@project}/apk/cracked.apk\"`")
 
 	end
 
 	def apktool_decompile(original)
 
-		cmd = eval("`java -jar tools/apktool.jar d -f #{@projects+@project}/apk/original.apk -o #{@projects+@project}/reversed/original`")
+		cmd = eval("`java -jar tools/apktool.jar d -f \"#{@projects+@project}/apk/original.apk\" -o \"#{@projects+@project}/reversed/original\"`")
 		FileUtils::cp_r("#{@projects+@project}/reversed/original/*", "#{@projects+@project}/reversed/cracked")
 
 	end
@@ -109,7 +109,7 @@ class Tools
 	def apk_sign
 		return if not validOptions
 
-		cmd = eval("`tools/dex2jar/d2j-apk-sign.sh -f #{@projects+@project}/apk/cracked.apk -o #{@projects+@project}/apk/signed.apk`")
+		cmd = eval("`tools/dex2jar/d2j-apk-sign.sh -f \"#{@projects+@project}/apk/cracked.apk\" -o \"#{@projects+@project}/apk/signed.apk\"`")
 
 	end
 end
